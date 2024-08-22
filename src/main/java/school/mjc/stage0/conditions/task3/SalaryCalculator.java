804951476cd0ee4d03e66b5323a263ea5f920726
Dzmitry Bodry <megabodry@gmail.com>
2024-08-22 15:02:43 +0300

    Completed task 3.

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 0 && salary <= 10000) {
+            System.out.println(salary - salary * 15 / 100.0);
+        }
+        if (salary > 10000 && salary <= 20000) {
+            System.out.println(salary - salary * 18 / 100.0);
+        }
+        if (salary > 20000) {
+            System.out.println(salary - salary * 20 / 100.0);
+        }
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
